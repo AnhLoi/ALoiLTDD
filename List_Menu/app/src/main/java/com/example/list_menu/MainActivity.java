9427391636c341e2861ee3ec2af5b45ecb6c525e
AnhLoi <vothianhloi60@gmail.com>
2020-10-24 23:12:09 +0700

    menu

---
+++ List_Menu/app/src/main/java/com/example/list_menu/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.list_menu;
+
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    ProductAdapter adapter;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.lvProducts);
+        //Khởi tạo các sản phẩm
+        SaleManager saleManager = SaleManager.get();
+        saleManager.generateProducts();
+
+        //lấy các product từ class saleManager
+        ArrayList products = saleManager.getProducts();
+        adapter = new ProductAdapter(this, products);//khởi tạo adapter
+        lv.setAdapter(adapter);//hiển lên listview
+        lv.setOnItemLongClickListener((AdapterView.OnItemLongClickListener) new ItemLongClickRemove());
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // nếu btnAdd được click
+        if (id == R.id.btnAdd) {
+            //đến màng hình ProductActivity
+            Intent intent = new Intent(this, ProductActivity.class);
+            //tham số -1 tức ta không truyền 1 position của item nào cả
+            //ta mở ProductActivity để thêm sp mới
+            intent.putExtra(ProductActivity.EXTRA_POSITION, -1);
+            startActivityForResult(intent, 0);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    private class ItemLongClickRemove implements AdapterView.OnItemLongClickListener {
+        @Override
+        public boolean onItemLongClick(AdapterView parent, View view, final int position, long id) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+            alertDialogBuilder.setMessage("Bán có muốn xóa sản phẩm này!");
+            alertDialogBuilder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // xóa sp đang nhấn giữ
+                    SaleManager.get().getProducts().remove(position);
+                    //cập nhật lại listview
+                    adapter.notifyDataSetChanged();
+
+                }
+            });
+            alertDialogBuilder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //không làm gì
+                }
+            });
+            alertDialogBuilder.show();
+            return true;
+        }
+    }
+
+
+}
